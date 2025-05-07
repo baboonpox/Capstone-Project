caed307483821c574602a5240b76bf190fc56d58
Brandon <barndonbandz@gmail.com>
2025-05-07 00:41:40 +0000

    responsiveness and navbar

--- navbar.js
+++ navbar.js
@@ -4,5 +4,14 @@ burger.addEventListener("click", openmenu);
 function openmenu(event) {
   let leftMenu = document.getElementsByClassName("left-nav")[0];
 
-  leftMenu.style.display = "block";
+  leftMenu.style.visibility = "visible";
+}
+
+let closebutton = document.getElementsByClassName("closebutton")[0];
+closebutton.addEventListener("click", closemenu);
+
+function closemenu(event) {
+  let leftMenu = document.getElementsByClassName("left-nav")[0];
+
+  leftMenu.style.visibility = "hidden";
 }
